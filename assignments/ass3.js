a9777de73f83fe9d2f0edc32eaf2fb4884dba215
home <home@Zaids-MacBook-Pro.local>
2022-09-04 23:43:47 +0530

    variable assignment

---
+++ assignments/ass3.js
@@ -0,0 +1,6 @@
+var name="owais";
+console.log(name);
+console.log(typeof(name));
+var age=20;
+console.log(age);
+console.log(typeof(age))
---
+++ assignments/ass4.js
@@ -0,0 +1,26 @@
+let name="owais";
+let school=`
+░██████╗██████╗░░██████╗
+██╔════╝██╔══██╗██╔════╝
+╚█████╗░██████╦╝╚█████╗░
+░╚═══██╗██╔══██╗░╚═══██╗
+██████╔╝██████╦╝██████╔╝
+╚═════╝░╚═════╝░╚═════╝░`;
+let grade="B";
+let section="c";
+let rollno=49;
+let marks_1=99;
+let marks_2=46;
+let marks_3=53
+console.log(name);
+console.log(school);
+console.log(grade);
+console.log(section);
+console.log(rollno);
+console.log(marks_1);
+console.log(marks_2);
+console.log(marks_3);
+
+
+
+
